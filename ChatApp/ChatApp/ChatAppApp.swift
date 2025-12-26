561fcdb460b7bd76c7acfc6492e0a7c58bd85c33
vadym01 <vasylaki95@gmail.com>
2025-12-26 13:05:06 +0200

    blueprint

---
+++ ChatApp/ChatApp/ChatAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Vadym Vasylaki on 26.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+  
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+               
+        }
+    }
+}
---
+++ ChatApp/ChatApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ChatApp
+//
+//  Created by Vadym Vasylaki on 26.12.2025.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+   
+
+    var body: some View {
+        NavigationView {
+           
+        }
+    }
+
+}
+
+
+#Preview {
+    ContentView()
+}
